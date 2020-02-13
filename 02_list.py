cd6647ed068a8283206871b82ade033a3a8e502d
Aircoral <lxr0528@126.cocm>
2020-02-13 22:06:31 +0800

    第二次提交

---
+++ 02_list.py
@@ -0,0 +1,77 @@
+#coding = utf-8
+#list 操作测试
+from timeit import Timer
+# 两个list相加操作
+# li1 = [1,2]
+# li2 = [23,5]
+# #产生新的列表三种方法
+# #1.两个列表相加
+# li = li1 + li2
+# #2.列表生成器
+# li = [i for i in range(10000)]
+# #3.将range（10000）转换成列表
+# li = list (range(10000))
+
+def test1():
+    li = []
+    for i in range (10000):
+        li.append(i)
+
+def test2():
+    li = []
+    for i in range (10000):
+        li = li+[i]   #li +=[i]
+
+def test3():
+    li =[i for i in range(10000)]
+
+def test4():
+    li = list(range(10000))
+
+def test5():
+    li = []
+    for i in range(10000):
+        li.extend(i) #extend与+操作的区别是：+操作产生新list，extend在原列表进行操作，接收列表或可遍历对象，append只能添加一个元素
+
+#构造测算timer类
+timer1 = Timer("test1()", "from __main__ import test1") #传参数时只接受字符串，不能传函数名
+print("append:", timer1.timeit(1000))
+
+timer2 = Timer("test2()", "from __main__ import test2") #传参数时只接受字符串，不能传函数名
+print("+:", timer2.timeit(1000))
+
+timer3 = Timer("test3()", "from __main__ import test3") #传参数时只接受字符串，不能传函数名
+print("[i for i in range:]", timer3.timeit(1000))
+
+timer4 = Timer("test4()", "from __main__ import test4") #传参数时只接受字符串，不能传函数名
+print("list(range():", timer4.timeit(1000))
+
+timer5 = Timer("test5()", "from __main__ import test5") #传参数时只接受字符串，不能传函数名
+print("extend:", timer4.timeit(1000))
+#执行结果：
+# append: 1.244506514
+# +: 199.144434223
+# [i for i in range:] 0.453316424999997
+# list(range(): 0.20962478900000292
+# extend: 0.20680837499998006
+
+
+def test6():
+    li = []
+    for i in range(10000):
+        li.append(i)#append只往表尾加
+
+def test7():
+    li = []
+    for i in range(10000):
+        li.insert(0, i) #0代表往列表头追加
+
+timer6 = Timer("test6()", "from __main__ import test6") #传参数时只接受字符串，不能传函数名
+print("append:", timer6.timeit(1000))
+
+timer7 = Timer("test7()", "from __main__ import test7") #传参数时只接受字符串，不能传函数名
+print("insert:", timer7.timeit(1000))
+
+#执行结果：
+# append: 1.004758061
+# insert: 28.37967281
--- README.md
+++ README.md
@@ -12,6 +12,50 @@
 #### 4.分支结构，时间复杂度取最大值
 #### 5.判断-个算法的效率时，往往只需要关注操作数量的最高次项，其它次要项和常数项可以忽略
 #### 6.在没有特殊说明时，我们所分析的算法的时间复杂度都是指最坏时间复杂度
+### 时间复杂度比较：O(1) < O(logn) < O(n) < O(nlogn) < O(n^2) < O(n^3) < O(2^n) < O(n!) < O(n^n)
+### 函数是对基本步骤的封装，其复杂度要看函数体里面的基本操作数，例如比较list.append()和list.insert()的时间效率。
+## 02Python内置类型性能分析
+### timeit模块
+### timeit模块可以用来测试一小段Python代码的执行速度。
+### class timeit. Timer(stmt='pass', setup='pass', timer= <timer function>)
+### Timer是测量小段代码执行速度的类。
+### stmt参数是要测试的代码语句(statment) ;
+### setup参数是运行代码时需要的设置;
+### timer参数是一个定时器函数， 与平台有关。
+### timeit.Timer.timeit(number=1000000)
+### Timer类中测试语句执行速度的对象方法。number参 数是测试代码时的测试次数，默认为1000000次。 方法
+### 返回执行代码的平均耗时，一个float类 型的秒数。
+### list内置操作的时间复杂度
+
+
+| Operation | Big-O Efficiency|
+| ------ | ------ |
+| indexx[] | O(1) |
+| index assignment | O(1) |
+| append | O(1)|
+| popO | O(1) |
+| pop(i) | O(n)|
+| insert(i,item)| O(n)|
+| del operator | O(n)|
+| iteration | O(n)|
+| contains (in) | O(n)|
+| get slice [x:) | O(k)|
+| del slice | O(n)|
+| set slice | O(n+ k)|
+| reverse | O(n)|
+| concatenate | O(k)|
+| sort | O(n logn)|
+| multiply | O(nk)
+
+### dict内置操作的时间复杂度
+| Operation | Big-O Efficiency|
+| ------ | ------ |
+|  |  |
+|  |  |
+|  |  |
+|  |  |
+|  |  |
+
 =======
 >>>>>>> 60593db7f464e2c7c0f6a88b87b4b445a586ce79
 #python_DataStructure
